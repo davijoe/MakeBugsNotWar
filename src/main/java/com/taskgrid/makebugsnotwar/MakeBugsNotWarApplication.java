51ba55f96606cf32e3c578fcff59c95e76501c20
Dan <danx8076@stud.kea.dk>
2023-05-01 11:51:11 +0200

    First commit

---
+++ src/main/java/com/taskgrid/makebugsnotwar/MakeBugsNotWarApplication.java
@@ -0,0 +1,13 @@
+package com.taskgrid.makebugsnotwar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeBugsNotWarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakeBugsNotWarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/taskgrid/makebugsnotwar/MakeBugsNotWarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskgrid.makebugsnotwar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeBugsNotWarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
