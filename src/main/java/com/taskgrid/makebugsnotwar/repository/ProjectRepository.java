fa52ee788ff9adda10a4fe563f90112d022861ed
Selmerr <115707577+Selmerr@users.noreply.github.com>
2023-05-04 12:13:38 +0200

    Fixed merge i messed up

--- src/main/java/com/taskgrid/makebugsnotwar/repository/ProjectRepository.java
+++ src/main/java/com/taskgrid/makebugsnotwar/repository/ProjectRepository.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Repository;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import org.springframework.stereotype.Repository;
 
 
 
