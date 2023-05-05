0c6c6b3cc701554862c3c7f502a05105c3c3a92d
jakobagger <113458342+jakobagger@users.noreply.github.com>
2023-05-05 08:23:19 +0200

    flyttet link

--- src/main/java/com/taskgrid/makebugsnotwar/repository/TaskRepository.java
+++ src/main/java/com/taskgrid/makebugsnotwar/repository/TaskRepository.java
@@ -1,7 +1,14 @@
 package com.taskgrid.makebugsnotwar.repository;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class TaskRepository {
+    @Value("${spring.datasource.url}")
+    private String DB_URL;
+    @Value("${spring.datasource.username}")
+    private String DB_UID;
+    @Value("${spring.datasource.password}")
+    private String DB_PWD;
 }
