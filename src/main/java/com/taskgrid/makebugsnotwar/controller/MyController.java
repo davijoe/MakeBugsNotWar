24b3b2b401dd46e24304d7c36174a349a34d9c71
jakobagger <113458342+jakobagger@users.noreply.github.com>
2023-05-08 00:28:04 +0200

    rebasing onto master

--- src/main/java/com/taskgrid/makebugsnotwar/controller/MyController.java
+++ src/main/java/com/taskgrid/makebugsnotwar/controller/MyController.java
@@ -11,6 +11,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -169,6 +170,11 @@ public class MyController {
             return "redirect:/login";
         }
 
+    }
+
+    @GetMapping("/project/{id}")
+    public String viewProject(@PathVariable("id") int projectId, Model model){
+        Project project = projectRepository.findProjectById(projectId);
     }
 
 
