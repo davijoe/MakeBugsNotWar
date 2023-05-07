08bd4d799ca8f597ab3a067576f7793051e2fb40
Dan Jørgensen <61663729+davijoe@users.noreply.github.com>
2023-05-08 00:43:03 +0200

    created task and task repo

--- src/main/java/com/taskgrid/makebugsnotwar/model/Task.java
+++ src/main/java/com/taskgrid/makebugsnotwar/model/Task.java
@@ -1,4 +1,41 @@
 package com.taskgrid.makebugsnotwar.model;
 
 public class Task {
+    private int taskId;
+    private String taskName;
+    private int taskTime;
+    private String taskDescription;
+
+    public Task(){} // default constructor
+
+    public Task(int taskId, String taskName, String taskDescription) {
+        this.taskId = taskId;
+        this.taskName = taskName;
+        this.taskDescription = taskDescription;
+    }
+
+    public int getTaskId() {
+        return taskId;
+    }
+    public void setTaskId(int taskId) {
+        this.taskId = taskId;
+    }
+    public String getTaskName() {
+        return taskName;
+    }
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+    public int getTaskTime(){
+        return taskTime;
+    }
+    public void setTaskTime(int taskTime){
+        this.taskTime = taskTime;
+    }
+    public String getTaskDescription(){
+        return taskDescription;
+    }
+    public void setTaskDescription(String taskDescription){
+        this.taskDescription = taskDescription;
+    }
 }
--- src/main/java/com/taskgrid/makebugsnotwar/repository/TaskRepository.java
+++ src/main/java/com/taskgrid/makebugsnotwar/repository/TaskRepository.java
@@ -1,7 +1,11 @@
 package com.taskgrid.makebugsnotwar.repository;
 
+import com.taskgrid.makebugsnotwar.model.Task;
+import com.taskgrid.makebugsnotwar.utility.ConnectionManager;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
+
+import java.sql.*;
 
 @Repository
 public class TaskRepository {
@@ -11,4 +15,33 @@ public class TaskRepository {
     private String DB_UID;
     @Value("${spring.datasource.password}")
     private String DB_PWD;
+
+    public int addTask(Task task) {
+        final String ADD_TASK_QUERY = "INSERT INTO taskgrid.tasks"+
+                "(task_name, task_time, task_description) VALUES (?,?,?)";
+
+        final String LAST_INSERT_QUERY = "SELECT LAST_INSERT_ID()";
+
+        int taskId = 0;
+
+        try{
+            Connection connection = ConnectionManager.getConnection(DB_URL, DB_UID, DB_PWD);
+        PreparedStatement preparedStatement = connection.prepareStatement(ADD_TASK_QUERY);
+        preparedStatement.setString(1, task.getTaskName());
+        preparedStatement.setInt(2, task.getTaskTime());
+        preparedStatement.setString(3, task.getTaskDescription());
+        preparedStatement.executeUpdate();
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(LAST_INSERT_QUERY);
+            resultSet.next();
+            taskId = resultSet.getInt(1);
+
+        } catch (SQLException e){
+            System.out.println("Could not add task to database");
+            e.printStackTrace();
+        }
+
+        return taskId;
+    }
 }
