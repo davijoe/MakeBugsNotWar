68fa09d925b45e4d62f2117f2b59134a506aacaa
jakobagger <113458342+jakobagger@users.noreply.github.com>
2023-05-08 00:28:04 +0200

    implement findProjectById()

--- src/main/java/com/taskgrid/makebugsnotwar/controller/MyController.java
+++ src/main/java/com/taskgrid/makebugsnotwar/controller/MyController.java
@@ -10,6 +10,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -140,6 +141,11 @@ public class MyController {
             return "redirect:/login";
         }
 
+    }
+
+    @GetMapping("/project/{id}")
+    public String viewProject(@PathVariable("id") int projectId, Model model){
+        Project project = projectRepository.findProjectById(projectId);
     }
 
 
