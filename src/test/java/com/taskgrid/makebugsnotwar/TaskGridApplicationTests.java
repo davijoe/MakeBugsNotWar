56049df539a8f8ffc01c4a2fdc4488a9f1a5dfc2
zivbabs <117026503+zivbabs@users.noreply.github.com>
2023-05-26 09:50:30 +0200

    Changed name of main class

--- src/test/java/com/taskgrid/makebugsnotwar/TaskGridApplicationTests.java
+++ src/test/java/com/taskgrid/makebugsnotwar/TaskGridApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = ControllerServicesTest.class)
-class MakeBugsNotWarApplicationTests {
+class TaskGridApplicationTests {
 
     @Test
     void contextLoads() {
